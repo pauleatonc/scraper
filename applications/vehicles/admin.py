d49fd8ba08d154708e36e2f2af70be99fe1ca3ef
paul <pauleatonc@gmail.com>
2024-08-22 06:02:25 -0400

    ADD: Adding models to django admin views

--- applications/vehicles/admin.py
+++ applications/vehicles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Vehicle, SellerType, BodyStyle, Brand, VehicleModel
 
-# Register your models here.
+# Registrar el modelo Vehicle en el admin
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('listing_id', 'title', 'price', 'scraped_at')  # Columnas que se mostrarán en la lista
+    search_fields = ('listing_id', 'title')  # Barra de búsqueda por listing_id o título
+    list_filter = ('price', 'scraped_at')  # Filtros en el admin
+
+admin.site.register(SellerType)
+admin.site.register(BodyStyle)
+admin.site.register(Brand)
+admin.site.register(VehicleModel)
+
